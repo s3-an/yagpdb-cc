0bc2240889b3ca77b85edb9802bad201436db56b
s3-an <75142387+s3-an@users.noreply.github.com>
2020-11-28 03:53:44 +0700

    Update scheduleUnique.cc.lua

--- Reminders/EPIC RPG/scheduleUnique.cc.lua
+++ Reminders/EPIC RPG/scheduleUnique.cc.lua
@@ -1,3 +1,11 @@
+{{/*
+        Trigger for Epic RPG Reminder
+
+	Recommended trigger: Recomended Trigger `none`
+
+
+*/}}
+
 {{ $msgId := (toString (sendMessageRetID nil .ExecData.message)) }}
 {{ dbSet .User.ID .ExecData.key $msgId }}
 {{ deleteMessage nil $msgId (mult 24 60 60) }}
